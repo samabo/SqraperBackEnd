c163ac601463b9352eb286c55e9fe81289b3103d
lbm-a <lynnarts11@gmail.com>
2018-12-06 14:07:30 +0100

    De mooiste upload van je leven

--- src/main/java/com/qien/sqraper/config/JerseyConfig.java
+++ src/main/java/com/qien/sqraper/config/JerseyConfig.java
@@ -6,12 +6,14 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 import com.qien.sqraper.api.SqraperEndpoint;
+import com.qien.sqraper.api.VacatureEndpoint;
 
 @Component
 @ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
 	public JerseyConfig() {
 		register(SqraperEndpoint.class);
+		register(VacatureEndpoint.class);
 	}
 
 }
